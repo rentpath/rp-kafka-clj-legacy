818d69d61ad65e37c597d3da9752de5776c2408b
Jenkins Service <jenkins@jenkins-slave003.tools.useast2.rentpath.com>
2019-05-08 12:01:21 -0400

    Version 0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/rp-kafka-clj "0.0.3"
+(defproject com.rentpath/rp-kafka-clj "0.0.4-SNAPSHOT"
   :description "Generic Clojure Kafka components and utils"
   :url "https://gitthub.com/rentpath/rp-kafka-clj"
   :license {:name "Eclipse Public License"
